25e163ab0c5041101d9611eb8686666bb44cdf89
Sebastian Rojas Gomez <88469107+Petulio14@users.noreply.github.com>
2024-03-16 23:57:34 -0500

    listo

--- Dealership/App.tsx
+++ Dealership/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Catalog from './src/Screens/Catalog'
-import ServiceHistory from './src/Screens/ServiceHistory'
+import * as React from 'react';
+//import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Catalog from './src/Screens/Catalog';
+import HomeScreen from './src/Screens/HomeScreen';
+import TecnicoScreeen from './src/Screens/Tecnico';
+import AsesoriaScreeen from './src/Screens/SoliticutdInfo';
 
-const App = () => {
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <Catalog/>
-    //<ServiceHistory/>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="INICIO" component={HomeScreen} />
+        <Stack.Screen name="CATALOGO" component={Catalog} />
+        <Stack.Screen name="TECNICO"   component={TecnicoScreeen} />
+        <Stack.Screen name ="ASESORIA" component={AsesoriaScreeen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default App
+
+
+export default App;
